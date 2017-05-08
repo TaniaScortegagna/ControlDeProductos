4a3d11a4ce7a3ccbcda1ba05bff479b9d156c2cf
TaniaScorte <tania.scortegagna14@gmail.com>
2017-05-08 18:19:47 -0300

    Arreglos en el Form

---
+++ ControlProductos/ControlProductos/DAO/RubroDao.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlProductos
+{
+    class RubroDao
+    {    
+
+        public RubroDao() { }
+        public List<Rubro> obtenerRubro(int pIdFlia)
+        {
+            List<Rubro> listaRubros = new List<Rubro>();
+            Conexion cnn = new Conexion();
+            string pConsulta = string.Format("SELECT IdRubro , Nombre FROM Rubros WHERE IdFlia = '{0}'", pIdFlia);            
+            DataTable resultado = cnn.EjecutarQuery(pConsulta, CommandType.Text);
+
+            for (int x = 0; x < resultado.Rows.Count; x++)
+            {
+                Rubro oRubro = new Rubro();
+                oRubro.Id = Convert.ToInt32(resultado.Rows[x]["IdRubro"]);
+                oRubro.Nombre = resultado.Rows[x]["Nombre"].ToString();
+                listaRubros.Add(oRubro);
+            }
+            cnn.Desconectar();
+            return listaRubros;
+        }
+    }
+}
---
+++ ControlProductos/ControlProductos/DAO/UsuarioDao.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlProductos
+{
+    class UsuarioDao
+    {
+        public UsuarioDao() { }
+
+
+        public bool BuscarUsuario(string pUsuario,string pClave )
+        {
+            bool estaOk = false;
+            Conexion cnn = new Conexion();            
+            string pConsulta = "";
+            pConsulta = string.Format(@"SELECT IdUsuario FROM Usuarios WHERE Usuario = '{0}' AND Clave ='{1}'",pUsuario, pClave);
+            DataTable resultado = cnn.EjecutarQuery(pConsulta, CommandType.Text);
+            cnn.Desconectar();
+            return resultado.Rows.Count > 0;
+
+        }
+   }
+}
