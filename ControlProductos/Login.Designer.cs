56bb7e860209eb3d44a00e0ea2d459fc5be8b9d5
MedinaMatiasDaniel <matiasdanielmedina86@gmail.com>
2017-05-21 18:25:09 -0300

    Commit 1
    Commit de prueba

--- ControlProductos/Login.Designer.cs
+++ ControlProductos/Login.Designer.cs
@@ -105,7 +105,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ClientSize = new System.Drawing.Size(282, 151);
+            this.ClientSize = new System.Drawing.Size(285, 151);
             this.Controls.Add(this.txbclave);
             this.Controls.Add(this.txbusuario);
             this.Controls.Add(this.lblclave);
